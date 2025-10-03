3d42ea3af6e699abe6964c321798b537e3823068
GodsonFlinto <153415803+GodsonFlinto@users.noreply.github.com>
2025-10-03 21:01:42 +0530

    Created postlist page

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,14 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const postRoutes = require('./Routes/posts')
+const cors = require('cors')
 
 const app = express()
 const PORT = process.env.PORT || 8000
 
 //Middleware
 app.use(bodyParser.json())
+app.use(cors())
 
 //connect to moboDB
 mongoose.connect('mongodb://localhost:27017/blog')
